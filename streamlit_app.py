4e1443c288334308e752b32d187c8c4007659c95
Olivier Lattrez <olivierlattrez@DS-MB-004.local>
2024-09-27 16:04:59 +0200

    own streamlit frontend

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,27 +10,30 @@ This is a minimal, reproducible example of how to scrape the web with Selenium a
 Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
 """
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
+#with st.echo():
+    # @st.cache_resource
+    # def get_driver():
+    #     return webdriver.Chrome(
+    #         service=Service(
+    #             ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+    #         ),
+    #         options=options,
+    #     )
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
+    # options = Options()
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--headless")
+    #
+    # driver = get_driver()
+    # driver.get("http://example.com")
 
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
+    #st.code(driver.page_source)
 
-    driver = get_driver()
-    driver.get("http://example.com")
+# Title of the app
+st.title("Simple Email and Phone Number Input App")
 
-    st.code(driver.page_source)
+# Create input fields for email and phone number
+email = st.text_input("Enter your Name:")
+phone = st.text_input("Enter your phone number:")
+
+print(google_search(email))
