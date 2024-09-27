9bdbf6a595d783edeeb1dfd39000c04179f9610c
Olivier Lattrez <olivierlattrez@DS-MB-004.local>
2024-09-27 16:25:01 +0200

    added additional options to scraper

--- scraper.py
+++ scraper.py
@@ -29,6 +29,8 @@ def initialize_driver():
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     return webdriver.Chrome(
             service=Service(
